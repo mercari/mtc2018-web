ae45d8fdd3830e926768b1ad4e5470dfc865e7d7
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-01 00:12:58 +0900

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const { ANALYZE } = process.env;
 
 module.exports = withTypescript({
   webpack(config, options) {
