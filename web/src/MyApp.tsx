ac0748e4b50add75fcc456c35d33f0cafc759324
adwd <ecdt.mn@gmail.com>
2018-08-09 18:50:10 +0900

    remove redux

--- web/src/MyApp.tsx
+++ web/src/MyApp.tsx
@@ -1,16 +1,12 @@
 import App, { Container } from 'next/app';
 import * as React from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
 
 class MyApp extends App {
   public render() {
     const { Component, pageProps } = this.props;
     return (
       <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
       </Container>
     );
   }
--- web/src/top/index.ts
+++ web/src/top/index.ts
@@ -1,2 +1,1 @@
 export { default as Top } from './Top';
-export { default as topReducer } from './top.reducer';
