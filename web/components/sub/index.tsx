bd101920d73e5ce25f4f29c500bbcd3371d0f42a
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-11 19:50:55 +0900

    Fix sub page

--- web/components/sub/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  background-color: blue;
-`;
-
-export const Sub = () => <Wrapper>遷移サンプル用のサブページです</Wrapper>;
---
+++ web/containers/Sub.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background-color: blue;
+`;
+
+export const Sub = () => (
+  <Wrapper>遷移サンプル用のサブページです。不要なので後で消します。</Wrapper>
+);
