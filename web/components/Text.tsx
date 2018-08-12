28f17e3a56b3a34dc0c7dcbb65becb7deb561d06
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-12 16:40:18 +0900

    Add Text component

---
+++ web/components/Text.tsx
@@ -0,0 +1,31 @@
+import styled, { css } from 'styled-components';
+import { textStyles } from './styles';
+
+const getTextStyle = (level: string) => {
+  let style = textStyles.find(textStyle => {
+    return textStyle.level === level;
+  });
+
+  if (!style) {
+    style = textStyles[0];
+  }
+
+  return css`
+    font-size: ${style.size}px;
+    font-weight: ${style.weight};
+  `;
+};
+
+interface IProps {
+  level?: string;
+}
+
+const Text: React.SFC<IProps> = ({ level, ...rest }) => (
+  <Wrapper level={level!} {...rest} />
+);
+
+const Wrapper = styled.div`
+  ${({ level }: { level: string }) => getTextStyle(level)};
+`;
+
+export default Text;
--- web/components/styles.ts
+++ web/components/styles.ts
@@ -4,3 +4,15 @@ export const colors = {
   yuki: '#FFFFFF',
   kemuri: '#D8D8D8'
 };
+
+export const textStyles = [
+  { level: 'display7', size: 65, weight: 'bold' },
+  { level: 'display6', size: 45, weight: 'bold' },
+  { level: 'display5', size: 35, weight: 'bold' },
+  { level: 'display4', size: 24, weight: 'normal' },
+  { level: 'display3', size: 18, weight: 'bold' },
+  { level: 'display2', size: 18, weight: 'normal' },
+  { level: 'display1', size: 16, weight: 'bold' },
+  { level: 'caption', size: 14, weight: 'bold' },
+  { level: 'body', size: 14, weight: 'normal' }
+];
