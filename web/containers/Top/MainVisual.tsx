4088e7999edce4970fec1ec074794289dd4fb4b2
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-22 00:26:48 +0900

    Update MainVisual

--- web/containers/Top/MainVisual.tsx
+++ web/containers/Top/MainVisual.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { Button } from '../../components';
+import { Button, Text } from '../../components';
 import { colors } from '../../components/styles';
 
 const MainVisual = () => (
   <Wrapper>
     <EmptySpace />
+    <Date>Oct. 4th, 2018 thu</Date>
+    <Place>@Roppongi Academy hills</Place>
     <BuyButton>BUY TICKET</BuyButton>
   </Wrapper>
 );
@@ -24,6 +26,18 @@ const Wrapper = styled.div`
 
 const EmptySpace = styled.div`
   flex-grow: 1;
+`;
+
+const Date = styled(Text).attrs({
+  level: 'display5'
+})`
+  margin-bottom: 20px;
+`;
+
+const Place = styled(Text).attrs({
+  level: 'display3'
+})`
+  margin-bottom: 64px;
 `;
 
 const BuyButton = styled(Button)``;
