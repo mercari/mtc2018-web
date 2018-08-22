68962665d15887ae3b85a4a80ebcf6b2c4599a41
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-22 18:07:35 +0900

    Fix MainVisual style

--- web/containers/Top/MainVisual.tsx
+++ web/containers/Top/MainVisual.tsx
@@ -5,41 +5,60 @@ import { colors } from '../../components/styles';
 
 const MainVisual = () => (
   <Wrapper>
-    <EmptySpace />
+    <EmptySpace>
+      <Logo />
+    </EmptySpace>
     <Date>Oct. 4th, 2018 thu</Date>
     <Place>@Roppongi Academy hills</Place>
     <BuyButton>BUY TICKET</BuyButton>
+    <BottomArrow />
   </Wrapper>
 );
 
 const Wrapper = styled.div`
   height: 100vh;
+  min-height: 768px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   color: ${colors.yuki};
-  padding: 128px;
-
+  padding: 7.8vh; /* 60 / 768 */
   box-sizing: border-box;
 `;
 
 const EmptySpace = styled.div`
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  width: 280px;
+  height: 134px;
+  border: 1px solid rgba(255, 255, 255, 0.5);
 `;
 
 const Date = styled(Text).attrs({
   level: 'display5'
 })`
-  margin-bottom: 20px;
+  margin-bottom: 2.6vh; /* 20 / 768 */
 `;
 
 const Place = styled(Text).attrs({
   level: 'display3'
 })`
-  margin-bottom: 64px;
+  margin-bottom: 8.3vh; /* 64 / 768 */
 `;
 
-const BuyButton = styled(Button)``;
+const BuyButton = styled(Button)`
+  margin-bottom: 9.3vh; /* 72 / 768 */
+`;
+
+const BottomArrow = styled.img.attrs({
+  src: '../../static/images/arrow_bottom.svg'
+})``;
 
 export default MainVisual;
