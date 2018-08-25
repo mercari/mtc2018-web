24245cfa2a158ece1da93239d6e091b02b903c0c
Masahiro Sano <sabottenda@gmail.com>
2018-08-25 19:02:27 +0900

    add healthz handler

--- server/cmd/mtcserver/main.go
+++ server/cmd/mtcserver/main.go
@@ -6,5 +6,16 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello, 世界"))
 	})
+
+	// for kubenretes readiness probe
+	http.HandleFunc("/healthz/readiness", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	// for kubenretes liveness probe
+	http.HandleFunc("/healthz/liveness", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
