d32796fcd3eb1d8b72c55c86a01b140d3b7900da
vvakame <vvakame+dev@gmail.com>
2018-08-28 17:22:50 +0900

    go installを使う go buildの代わりに

--- server/setup.sh
+++ server/setup.sh
@@ -9,6 +9,7 @@ mkdir bin/
 go mod download
 # go mod tidy
 
-go build -o bin/goimports   golang.org/x/tools/cmd/goimports
-go build -o bin/golint      golang.org/x/lint/golint
-go build -o bin/gqlgen      github.com/99designs/gqlgen
+export GOBIN=`pwd -P`/bin
+go install golang.org/x/tools/cmd/goimports
+go install golang.org/x/lint/golint
+go install github.com/99designs/gqlgen
