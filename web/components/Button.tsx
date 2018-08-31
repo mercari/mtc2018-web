5febc8a5b94bca8d99f72af8dc8d138773ae6c85
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-08-31 19:50:30 +0900

    Fix button style

--- web/components/Button.tsx
+++ web/components/Button.tsx
@@ -1,14 +1,35 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors, getTextStyle } from './styles';
 
+interface Props {
+  type: 'primary' | 'secondary';
+}
+
+const getStyle = (type: 'primary' | 'secondary') => {
+  switch (type) {
+    case 'secondary':
+      return css`
+        background-color: transparent;
+        border: 1px solid ${colors.secondary};
+        color: ${colors.secondary};
+      `;
+    case 'primary':
+    default:
+      return css`
+        background-color: ${colors.secondary};
+        border: none;
+        color: ${colors.yuki};
+      `;
+  }
+};
+
 const Button = styled.button`
-  ${getTextStyle('display2')};
-  background-color: ${colors.secondary};
-  color: ${colors.yuki};
-  border: none;
+  ${getTextStyle('display2')} ${(props: Props) =>
+    getStyle(props.type)}
   height: 60px;
   padding: 0 48px;
   border-radius: 32px;
+  box-sizing: border-box;
   cursor: pointer;
   transition: 0.3s;
   display: flex;
