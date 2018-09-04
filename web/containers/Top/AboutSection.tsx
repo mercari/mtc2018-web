571cece3a6d5f744e3aa1f760fb673a3548bc091
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-09-04 14:12:51 +0900

    Fix about

--- web/containers/Top/AboutSection.tsx
+++ web/containers/Top/AboutSection.tsx
@@ -7,13 +7,24 @@ const AboutSection: React.SFC<{}> = props => (
   <Section title="ABOUT" id="about" {...props}>
     <StyledCard>
       <Message>
-        Mercari Tech Conf（MTC）は、株式会社メルカリをはじめとするメルカリグループ各社が、これから目指す方向や、これから取り組む技術的なチャレンジについてご紹介するエンジニア向けの技術カンファレンスです。
+        <p>
+          Mercari Tech
+          Conf（MTC）は、株式会社メルカリをはじめとするメルカリグループ各社が、これから目指す方向や、これから取り組む技術的なチャレンジについてご紹介するエンジニア向けの技術カンファレンスです。
+        </p>
 
-        今年で第2回となる「Mercari Tech Conf 2018」では、テーマを「Evolution（変化）」として、この1年間でメルカリグループ内に起こった、もしくは起こりつつある変化をご紹介します。
+        <p>
+          今年で第2回となる「Mercari Tech Conf
+          2018」では、テーマを「Evolution（変化）」として、この1年間でメルカリグループ内に起こった、もしくは起こりつつある変化をご紹介します。
+        </p>
 
-        各セッションに加え、昨年好評だった各エンジニアチームによる展示ブース、そしてお食事やお飲み物をご用意したAfter Party（懇親会）もございますので、是非エンジニア同士の交流の場としてもご活用ください。
+        <p>
+          各セッションに加え、昨年好評だった各エンジニアチームによる展示ブース、そしてお食事やお飲み物をご用意したAfter
+          Party（懇親会）もございますので、是非エンジニア同士の交流の場としてもご活用ください。
+        </p>
 
-        なお、各セッションには日本語・英語の同時通訳をご用意しております。
+        <p>
+          なお、各セッションには日本語・英語の同時通訳をご用意しております。
+        </p>
       </Message>
     </StyledCard>
   </Section>
@@ -38,6 +49,15 @@ const Message = styled(Text).attrs({
   @media screen and (max-width: 767px) {
     ${getTextStyle('body')};
     line-height: 22px;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 24px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
   }
 `;
 
