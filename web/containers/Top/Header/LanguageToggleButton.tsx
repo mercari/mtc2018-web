17911269721d5168a55af7a4fa1af13f29274f20
SAWADA Takayoshi <sawadasuiren@gmail.com>
2018-09-10 19:20:13 +0900

    Add arrow icon

--- web/containers/Top/Header/LanguageToggleButton.tsx
+++ web/containers/Top/Header/LanguageToggleButton.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import { I18n } from 'react-i18next';
 import { Button } from '../../../components';
 import { isJapan } from '../../../utils';
@@ -10,15 +11,25 @@ class LanguageToggleButton extends React.PureComponent {
         {(_, { i18n }) => {
           const isJa = isJapan(i18n.language);
           const onClick = () => i18n.changeLanguage(isJa ? 'en-US' : 'ja-JP');
+          const from = isJa ? 'JA' : 'EN';
+          const to = isJa ? 'EN' : 'JA';
           return (
-            <Button onClick={onClick} size="small" {...this.props}>
-              {isJa ? 'JA → EN' : 'EN → JA'}
-            </Button>
+            <Wrapper onClick={onClick} size="small" {...this.props}>
+              {from}
+              <img src="/static/images/icn_arrow.svg" />
+              {to}
+            </Wrapper>
           );
         }}
       </I18n>
     );
   }
 }
+
+const Wrapper = styled(Button)`
+  img {
+    margin: 0 8px 1px;
+  }
+`;
 
 export default LanguageToggleButton;
