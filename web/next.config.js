8c4271b8f4ab80bc62010d2c571a412620c558f4
adwd <ecdt.mn@gmail.com>
2018-09-11 18:37:26 +0900

    work offline

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,16 @@
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const withOffline = require('next-offline');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = withTypescript({
+module.exports = withTypescript(withOffline({
   webpack(config, options) {
-    if (options.isServer) { config.plugins.push(new ForkTsCheckerWebpackPlugin()); }
+    if (options.isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
+    }
+
+    config.plugins.push(new CopyWebpackPlugin(['./lib/sw.js']));
+
     return config;
   },
   serverRuntimeConfig: {
@@ -11,5 +18,15 @@ module.exports = withTypescript({
   },
   publicRuntimeConfig: {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
-  }
-});
+  },
+  workboxOpts: {
+    // next-offline default workboxOpts
+    // https://github.com/hanford/next-offline/blob/acc4f35ed18ddb55b4573315b469761696310663/index.js#L23-L30
+    globPatterns: ['static/**/*'],
+    globDirectory: '.',
+    runtimeCaching: [
+      { urlPattern: /^https?.*/, handler: 'networkFirst' }
+    ],
+    importScripts: ['/sw.js'],
+  },
+}));
