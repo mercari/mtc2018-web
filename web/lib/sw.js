efa9f91ae29c26888c4325d3a9a04451d4087492
adwd <ecdt.mn@gmail.com>
2018-09-11 20:19:27 +0900

    enable Offline Google Analytics and Google Maps

--- web/lib/sw.js
+++ web/lib/sw.js
@@ -1,1 +1,8 @@
 console.log('service worker loaded');
+
+// Enable Offline Google Analytics and Google Maps
+workbox.googleAnalytics.initialize();
+workbox.routing.registerRoute(
+  new RegExp('^https://maps.googleapis.com/maps/api/js*'),
+  workbox.strategies.staleWhileRevalidate()
+);
