be2b952e64162f7e21f35c9a962ed916371c1c92
vvakame <vvakame+dev@gmail.com>
2018-09-13 19:36:27 +0900

    typoみつけたのでついでに治す

--- web/graphql/generated/LikeSession.ts
+++ web/graphql/generated/LikeSession.ts
@@ -2,36 +2,36 @@
 // This file was automatically generated and should not be edited.
 
 // ====================================================
-// GraphQL mutation operation: LikeSesstion
+// GraphQL mutation operation: LikeSession
 // ====================================================
 
-export interface LikeSesstion_createLike_like_session {
+export interface LikeSession_createLike_like_session {
   __typename: "Session";
   id: string;
   liked: number;
 }
 
-export interface LikeSesstion_createLike_like {
+export interface LikeSession_createLike_like {
   __typename: "Like";
   id: string;
-  session: LikeSesstion_createLike_like_session;
+  session: LikeSession_createLike_like_session;
 }
 
-export interface LikeSesstion_createLike {
+export interface LikeSession_createLike {
   __typename: "CreateLikePayload";
   clientMutationId: string | null;
-  like: LikeSesstion_createLike_like;
+  like: LikeSession_createLike_like;
 }
 
-export interface LikeSesstion {
+export interface LikeSession {
   /**
    * セッションに対していいね！することができます。
    * ログイン周りのシステムはないので、リクエストにUUIDを付与してください（仕様未定）。
    */
-  createLike: LikeSesstion_createLike | null;
+  createLike: LikeSession_createLike | null;
 }
 
-export interface LikeSesstionVariables {
+export interface LikeSessionVariables {
   randomId: string;
   sessionId: string;
 }
--- web/graphql/mutation.ts
+++ web/graphql/mutation.ts
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag';
 
 export const LIKE_SESSION_MUTATION = gql`
-  mutation LikeSesstion($randomId: String!, $sessionId: ID!) {
+  mutation LikeSession($randomId: String!, $sessionId: ID!) {
     createLike(input: { clientMutationId: $randomId, sessionId: $sessionId }) {
       clientMutationId
       like {
